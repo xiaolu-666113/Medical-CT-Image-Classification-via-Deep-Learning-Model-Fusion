d9372c95372aa344ac9a13a3ee4bcb0f4f29628b
xiaolu-666113 <406987331@qq.com>
2025-06-11 16:15:40 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+batch_size = 4#2 4 8
+num_classes = 2
+num_epochs = 10
+lr = 1e-4
+image_size = 224
+data_dir = "covidData"
+
+model_names = ['resnet', 'densenet', 'efficientnet', 'vit', 'convnext', 'regnet', 'swin', 'mobilenetv3', 'beit']
+
+
